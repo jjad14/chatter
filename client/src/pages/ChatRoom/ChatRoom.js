a48bdd4e74311d1c14dd422cb5719a78b3f19940
Dennis <dmarlo96@live.ca>
2021-08-24 15:13:43 -0400

    User List Card

--- client/src/pages/ChatRoom/ChatRoom.js
+++ client/src/pages/ChatRoom/ChatRoom.js
@@ -6,15 +6,23 @@ import Divider from "@material-ui/core/Divider";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
 import IconButton from "@material-ui/core/IconButton";
 import SendIcon from "@material-ui/icons/Send";
+import Hidden from "@material-ui/core/Hidden";
+
+import UserList from "./UserList";
 
 const useStyles = makeStyles((theme) => ({
+	root: {
+		display: "flex",
+		flexDirection: "row"
+	},
+	gridContainer: {
+		flexGrow: 2
+	},
 	title: {
-		marginBottom: theme.spacing(2)
+		marginBottom: theme.spacing(2),
+		paddingLeft: theme.spacing(1)
 	},
 	chatSection: {
 		width: "100%",
@@ -26,6 +34,9 @@ const useStyles = makeStyles((theme) => ({
 	},
 	send: {
 		marginLeft: theme.spacing(1)
+	},
+	usersCard: {
+		flexGrow: 1
 	}
 }));
 
@@ -33,39 +44,49 @@ const ChatRoom = () => {
 	const classes = useStyles();
 
 	return (
-		<div>
-			<Grid container>
-				<Grid item xs={12}>
-					<Typography variant='h5' className={classes.title}>
-						Chat Room Name
-					</Typography>
+		<div className={classes.root}>
+			<div className={classes.gridContainer}>
+				<Grid container>
+					<Grid item xs={12}>
+						<Typography variant='h5' className={classes.title}>
+							Chat Room Name
+						</Typography>
+					</Grid>
 				</Grid>
-			</Grid>
-			<Grid container component={Paper} className={classes.chatSection}>
-				<Grid item xs={12}>
-					{/* Messages here */}
-					<List className={classes.messageArea}></List>
-					<Divider />
-					<Grid container style={{ padding: "20px" }}>
-						<Grid item xs={11}>
-							<TextField
-								id='outlined-basic-email'
-								label='Type Something'
-								fullWidth
-							/>
-						</Grid>
-						<Grid xs={1} align='right'>
-							{/* onClick, submit message */}
-							<IconButton
-								aria-label='send'
-								color='secondary'
-								className={classes.send}>
-								<SendIcon />
-							</IconButton>
+				<Grid
+					container
+					component={Paper}
+					className={classes.chatSection}>
+					<Grid item xs={12}>
+						{/* Messages here */}
+						<List className={classes.messageArea}></List>
+						<Divider />
+						<Grid container style={{ padding: "20px" }}>
+							<Grid item xs={10}>
+								<TextField
+									id='outlined-basic-email'
+									label='Type Something'
+									fullWidth
+								/>
+							</Grid>
+							<Grid xs={2} align='right'>
+								{/* onClick, submit message */}
+								<IconButton
+									aria-label='send'
+									color='secondary'
+									className={classes.send}>
+									<SendIcon />
+								</IconButton>
+							</Grid>
 						</Grid>
 					</Grid>
 				</Grid>
-			</Grid>
+			</div>
+			<Hidden smDown>
+				<div className={classes.usersCard}>
+					<UserList />
+				</div>
+			</Hidden>
 		</div>
 	);
 };
---
+++ client/src/pages/ChatRoom/UserList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+
+const useStyles = makeStyles({
+	root: {
+		height: "100%",
+		paddingLeft: 5
+	}
+});
+
+const UserList = () => {
+	const classes = useStyles();
+	return (
+		<div className={classes.root}>
+			<Card className={classes.root}>
+				<Typography component='h1' variant='h6' align='center'>
+					Users
+				</Typography>
+				<Divider />
+				<CardContent>
+					<List className={classes.userArea}></List>
+				</CardContent>
+			</Card>
+		</div>
+	);
+};
+
+export default UserList;
